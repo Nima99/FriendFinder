5271c765110605c182eff9b2d64b375edd8ec33b
Nima Akhaveissy <Nima1998@live.com>
2019-09-15 18:50:18 -0500

    added some friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,64 @@
+var friends = [
+    {
+        name:"Tom Hiddleston",
+        photo:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Tom_Hiddleston_%2827992938324%29_%28cropped%29.jpg/800px-Tom_Hiddleston_%2827992938324%29_%28cropped%29.jpg",
+        scores:[5, 1, 2, 3, 4, 5, 4, 3, 2, 1],
+    },
+    {
+        name:"Kanye West",
+        photo:
+        "https://imagez.tmz.com/image/5e/4by3/2019/09/06/5e7eb491f813446384652b5e9db572da_md.jpg",
+        scores:[4, 1, 3, 4, 4, 4, 1, 2, 2, 1],
+    },
+    {
+        name:"Bear Grills",
+        photo:
+        "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/09/06/15/bear-grylls.jpg",
+        scores:[3, 5, 5, 5, 4, 4, 2, 1, 4, 5],
+    },
+    {
+        name:"Frank Ocean",
+        photo:
+        "https://static.vibe.com/files/2018/11/frank-ocean-teases-new-music-1542647793-650x366.jpg",
+        scores:[2, 3, 2, 1, 1, 1, 4, 2, 3, 5],
+    },
+    {
+        name:"Kid Cudi",
+        photo:
+        "https://i.scdn.co/image/fe6bab948a4650b5ef340aaa0fb6545c4a048b99",
+        scores:[1, 2, 3, 4, 5, 4, 4, 3, 3, 2],
+    },
+    {
+        name:"Katy Perry",
+        photo:
+        "https://www.udiscovermusic.com/wp-content/uploads/2019/05/2018-Publicity-Shot-Katy-Perry-Photo-Credit_-Rony-Alwin-Secondary-web-optimised-1000.jpg",
+        scores:[5, 1, 2, 3, 4, 3, 1, 3, 2, 1],
+    },
+    {
+        name:"Mila Kunis",
+        photo:
+        "https://s.yimg.com/ny/api/res/1.2/UFnUa.H_fuopUGjYaf910w--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NjE4O2g9NDEy/http://media.zenfs.com/en_US/News/TheWrap/Mila_Kunis_Comedy_8216Bad_Moms8217-4f016565380295f7a31e387e45905c9f",
+        scores:[5, 3, 2, 3, 4, 3, 2, 3, 4, 4],
+    },
+    {
+        name:"Josh Radnor",
+        photo:
+        "https://bloximages.newyork1.vip.townnews.com/collegian.psu.edu/content/tncms/assets/v3/editorial/1/74/1746ec18-46cb-11e9-9518-d77d97b37568/5c8b0f99710f5.image.jpg?resize=400%2C386",
+        scores:[5, 1, 2, 3, 4, 5, 3, 4, 2, 2],
+    },
+    {
+        name:"Morgan Freeman",
+        photo:
+        "http://www.bu.edu/files/2013/05/v2_MF-Headshot-030813.jpg",
+        scores:[5, 1, 2, 1, 5, 3, 2, 2, 4, 5],
+    },
+    {
+        name:"Kate Upton",
+        photo:
+        "https://pbs.twimg.com/media/DfHYaGGVQAArGTV.jpg",
+        scores:[1, 1, 2, 3, 4, 5, 5, 5, 5, 5],
+    },
+];
+
+module.exports = friends;
